cac01f1b19b7128e8a75cfa86eca5c5475d80aa1
Fahmi <fahmiali130499@gmail.com>
2023-05-06 14:10:29 +0700

    clean

--- src/components/pages/about/detail/DesignTabs.tsx
+++ src/components/pages/about/detail/DesignTabs.tsx
@@ -7,21 +7,19 @@ interface AboutDetailProps {
 }
 function DesignTabs({ bright }: AboutDetailProps) {
   return (
-    <div>
-      <div className="grid grid-cols-2 sm:grid-cols-5 gap-4 ">
-        {design.map((item, idx) => (
-          <div
-            key={idx}
-            className={`${
-              bright
-                ? "border border-gray-700 bg-slate-800 text-white rounded-lg p-3"
-                : "border border-gray-200 bg-white rounded-lg p-3"
-            }`}
-          >
-            <SoftSkill title={item.title} icons={<item.Icons />} />
-          </div>
-        ))}
-      </div>
+    <div className="grid grid-cols-2 sm:grid-cols-5 gap-4 ">
+      {design.map((item, idx) => (
+        <div
+          key={idx}
+          className={`${
+            bright
+              ? "border border-gray-700 bg-slate-800 text-white rounded-lg p-3"
+              : "border border-gray-200 bg-white rounded-lg p-3"
+          }`}
+        >
+          <SoftSkill title={item.title} icons={<item.Icons />} />
+        </div>
+      ))}
     </div>
   );
 }
