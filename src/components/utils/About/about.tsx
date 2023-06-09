301caa9589dbe0add8397ea9bbde9c08c26f468e
Fahmi <fahmiali130499@gmail.com>
2023-06-10 05:02:27 +0700

    update semester

--- src/components/utils/About/about.tsx
+++ src/components/utils/About/about.tsx
@@ -7,7 +7,7 @@ export const about = [
       {
         key: 1,
         subAbout:
-          "Hi, my name is Fahmi Ali Husni, you can call me Fahmi. I was born in Banyuwangi, East Java, Indonesia. I currently live in Malang. I have experience as a web developer, particularly in frontend and UI/UX design. I am currently in my seventh semester of studying computer science at the University of Muhammadiyah Malang and have a passion for development and technology, including coding, research, and competitive programming.",
+          "Hi, my name is Fahmi Ali Husni, you can call me Fahmi. I was born in Banyuwangi, East Java, Indonesia. I currently live in Malang. I have experience as a web developer, particularly in frontend and UI/UX design. I am currently in my eight semester of studying computer science at the University of Muhammadiyah Malang and have a passion for development and technology, including coding, research, and competitive programming.",
       },
       {
         key: 2,
