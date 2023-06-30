42fa52995aa59ff8421cf4c21842828fff6bbfc3
Fahmi <fahmiali130499@gmail.com>
2023-06-30 19:12:41 +0700

    upt about

--- src/components/utils/About/contact.tsx
+++ src/components/utils/About/contact.tsx
@@ -1,38 +1,31 @@
 export const contact = [
   {
-    id: 1,
     title: "Contact",
     about:
       "If you want to collaborate on a project related to technology, you can contact me through social media below. Thank you for visiting my profile.",
     contact: [
       {
-        id: 1,
         title: "Email",
         href: "mailto:fahmiali130499@gmail.com",
       },
       {
-        id: 2,
         title: "Github",
         href: "https://github.com/Fahmiali99",
       },
       {
-        id: 3,
         title: "Instagram",
         href: "https://instagram.com/fahmi_ali13/",
       },
 
       {
-        id: 4,
         title: "Dribbble",
         href: "https://dribbble.com/Fahmiali13",
       },
       {
-        id: 5,
         title: "Linkedin",
         href: "https://linkedin.com/in/fahmialihusni/",
       },
       {
-        id: 6,
         title: "Spotify",
         href: "https://open.spotify.com/user/xa0c9m7olx3ucvbyp2atz2t78",
       },
--- src/components/utils/About/design.tsx
+++ src/components/utils/About/design.tsx
@@ -9,27 +9,22 @@ import { FaFigma } from "react-icons/fa";
 
 export const design = [
   {
-    id: 1,
     title: "Figma",
     Icons: FaFigma,
   },
   {
-    id: 2,
     title: "Adobe XD",
     Icons: SiAdobexd,
   },
   {
-    id: 3,
     title: "Adobe PS",
     Icons: SiAdobephotoshop,
   },
   {
-    id: 4,
     title: "Adobe AI",
     Icons: SiAdobeillustrator,
   },
   {
-    id: 5,
     title: "Adobe Pr",
     Icons: SiAdobepremierepro,
   },
