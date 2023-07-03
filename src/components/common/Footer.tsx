2dd226ece2c730884f07145295fa12dc2bd69d64
Fahmi <fahmiali130499@gmail.com>
2023-07-03 07:37:52 +0700

    upt footer v2

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,37 +1,35 @@
 import React from "react";
-
 import { data } from "../utils/footer";
 
 function Footer() {
   return (
-    <div className="" style={{ height: "48px" }}>
-      <div className="  px-2 sm:px-0 py-2.5 ">
-        <div className="grid grid-cols-1 xl:grid-cols-2">
-          <div className="w-full ">
-            <span className="hidden xl:block ">
-              © 2022
-              <a href="/" className="hover:underline">
-                Fahmi Ali Husni
-              </a>
-              . All Rights Reserved.
-            </span>
-          </div>
-          <div className="w-full flex justify-center xl:justify-end">
-            {data.map((item, idx) => {
-              return (
-                <div key={idx} className="mx-2">
-                  <button
-                    onClick={() => window.open(item.href)}
-                    className="flex items-center"
-                  >
-                    <item.Icons className="mx-2" />
-                    <p>{item.title}</p>
-                  </button>
-                </div>
-              );
-            })}
-          </div>
-        </div>
+    <div
+      className="w-full grid grid-cols-1 xl:grid-cols-2 px-2 sm:px-0 py-2.5"
+      style={{ height: "48px" }}
+    >
+      <div>
+        <span className="hidden xl:block ">
+          © 2022
+          <a href="/" className="hover:underline">
+            Fahmi Ali Husni
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
+      <div className="flex justify-center xl:justify-end">
+        {data.map((item, idx) => {
+          return (
+            <div key={idx} className="mx-2">
+              <button
+                onClick={() => window.open(item.href)}
+                className="flex items-center"
+              >
+                <item.Icons className="mx-2" />
+                <p>{item.title}</p>
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
