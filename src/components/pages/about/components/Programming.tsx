9183650d8a263de00c14749a5efd6b2e776059a7
Fahmi Ali Husni <fahmiali130499@gmail.com>
2023-07-17 21:08:59 +0700

    upt

--- src/components/pages/about/components/Programming.tsx
+++ src/components/pages/about/components/Programming.tsx
@@ -5,12 +5,10 @@ interface CompAbout {
 }
 function Programming({ title, icons }: CompAbout) {
   return (
-    <div>
-      <div className="w-full flex justify-center">
-        <div>
-          <h1 className=" text-6xl w-full flex justify-center">{icons}</h1>
-          <h1 className=" text-center">{title}</h1>
-        </div>
+    <div className="w-full flex justify-center">
+      <div>
+        <h1 className=" text-6xl w-full flex justify-center">{icons}</h1>
+        <h1 className=" text-center">{title}</h1>
       </div>
     </div>
   );
