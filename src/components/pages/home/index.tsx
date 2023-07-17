61c54d96f4a0fdc190e8f5168de7f24d27bdaede
Fahmi Ali Husni <fahmiali130499@gmail.com>
2023-07-17 21:06:27 +0700

    upt

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -22,7 +22,6 @@ function Home({ bright }: HomeProps) {
           <div className="flex justify-center pt-2">
             <a
               href="/resume"
-              target="_blank"
               className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
             >
               Resume
