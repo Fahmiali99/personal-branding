c86e80012972c0b172f826003d70b282f22520ed
Fahmi Ali Husni <fahmiali130499@gmail.com>
2023-10-30 21:05:35 +0700

    fix: improve_performance

--- src/components/pages/portfolio/index.tsx
+++ src/components/pages/portfolio/index.tsx
@@ -1,8 +1,8 @@
-import React, { useMemo, useState } from "react";
+import React, { Fragment, useEffect, useMemo, useState } from "react";
 import { data } from "../../utils/Portfolio/data";
-import DetailPortfolio from "./detail/DetailPortfolio";
-import Pagination from "../../common/Pagination";
-import Searching from "../../common/Searching";
+const DetailPortfolio = React.lazy(() => import('./detail/DetailPortfolio'));
+const Pagination = React.lazy(() => import('../../common/Pagination'));
+const Searching = React.lazy(() => import('../../common/Searching'));
 
 interface PortfolioProps {
   bright: boolean;
@@ -34,62 +34,69 @@ function Portfolio({ bright }: PortfolioProps) {
     e.preventDefault();
   }
 
+  useEffect(() => {
+    document.title = 'Hi, Fahmi | Portfolio';
+  }, []);
+
   return (
-    <div className="pt-28 pb-4 w-full  xl:min-h-[calc(100vh-3rem)] px-2 md:px-0">
-      <div className="text-center flex justify-center">
-        <div>
-          <h1 className="text-2xl sm:text-3xl font-medium">Portfolio</h1>
-          <p className="py-5">
-            Discover appropriate portfolios to guide you towards your desired
-            selection.
-          </p>
-          <div className="pb-1 sm:pb-2">
-            <Searching
-              handleSubmit={handleFilterSubmit}
-              filter={filter}
-              handleFilter={handleFilterChange}
-            />
+    <Fragment>
+      <head>Portfolio</head>
+      <div className="pt-28 pb-4 w-full  xl:min-h-[calc(100vh-3rem)] px-2 md:px-0">
+        <div className="text-center flex justify-center">
+          <div>
+            <h1 className="text-2xl sm:text-3xl font-medium">Portfolio</h1>
+            <p className="py-5">
+              Discover appropriate portfolios to guide you towards your desired
+              selection.
+            </p>
+            <div className="pb-1 sm:pb-2">
+              <Searching
+                handleSubmit={handleFilterSubmit}
+                filter={filter}
+                handleFilter={handleFilterChange}
+              />
+            </div>
           </div>
         </div>
+
+        {currentTableData.length ? (
+          <div className=" grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4 my-5 ">
+            {currentTableData.map((item, idx) => {
+              const technology = item.technology;
+
+              const setLength = item.story;
+              let substring = setLength.substring(0, 108);
+              const maxLength = 107;
+              if (substring.length > maxLength) {
+                substring = substring.substring(0, maxLength) + "...";
+              }
+              return (
+                <DetailPortfolio
+                  key={idx}
+                  title={item.title}
+                  href={item.href}
+                  image={item.image}
+                  date={item.date}
+                  subString={substring}
+                  technology={technology}
+                  bright={bright}
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <div className=" min-h-[calc(57vh-49px)] xl:min-h-[calc(53vh-3rem)] flex items-center justify-center text-center">
+            Portfolio not available
+          </div>
+        )}
+        <Pagination
+          currentPage={currentPage}
+          totalCount={data.length}
+          pageSize={PageSize}
+          onPageChange={(page: number) => setCurrentPage(page)}
+        />
       </div>
-
-      {currentTableData.length ? (
-        <div className=" grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4 my-5 ">
-          {currentTableData.map((item, idx) => {
-            const technology = item.technology;
-
-            const setLength = item.story;
-            let substring = setLength.substring(0, 108);
-            const maxLength = 107;
-            if (substring.length > maxLength) {
-              substring = substring.substring(0, maxLength) + "...";
-            }
-            return (
-              <DetailPortfolio
-                key={idx}
-                title={item.title}
-                href={item.href}
-                image={item.image}
-                date={item.date}
-                subString={substring}
-                technology={technology}
-                bright={bright}
-              />
-            );
-          })}
-        </div>
-      ) : (
-        <div className=" min-h-[calc(57vh-49px)] xl:min-h-[calc(53vh-3rem)] flex items-center justify-center text-center">
-          Portfolio not available
-        </div>
-      )}
-      <Pagination
-        currentPage={currentPage}
-        totalCount={data.length}
-        pageSize={PageSize}
-        onPageChange={(page: number) => setCurrentPage(page)}
-      />
-    </div>
+    </Fragment>
   );
 }
 
