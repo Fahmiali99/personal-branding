8f6f318b71ced81cece7d4f1c1fceb9f478f3f3f
Fahmi Ali Husni <fahmiali130499@gmail.com>
2024-01-29 19:45:55 +0700

    fix: summary

--- src/components/utils/About/about.tsx
+++ src/components/utils/About/about.tsx
@@ -6,11 +6,11 @@ export const about = [
     about: [
       {
         subAbout:
-          "Hi, my name is Fahmi Ali Husni, you can call me Fahmi. I was born in Banyuwangi, East Java, Indonesia. I currently live in Malang. I have experience as a web developer, particularly in frontend and UI/UX design. I am currently in my eight semester of studying computer science at the University of Muhammadiyah Malang and have a passion for development and technology, including coding, research, and competitive programming.",
+          "Hi, my name is Fahmi Ali Husni, you can call me Fahmi. I was born in Banyuwangi, East Java, Indonesia. I currently live in Malang. Experienced in website development with a specialization in frontend developer, where i bring digital visions to life with captivating user interfaces and user experiences. I am highly passionate about creating visually stunning and intuitively responsive frontend designs that not only meet but also exceed user expectations. My dedication to continuously exploring innovations in frontend technology ensures that every project I undertake is not only functional but also aesthetically appealing according to user needs.",
       },
       {
         subAbout:
-          "I really enjoy web programming, particularly as a frontend developer, using HTML, PHP, JavaScript, TypeScript, ReactJS, and NextJS. I have been involved with the Computer Science Student Association, in the field of information and technology, as an audio podcast editor and the Google Developer Student Clubs Community at Muhammadiyah University of Malang, which I join with the goal of expanding my knowledge and gaining experience.",
+          "I really enjoy web programming, particularly as a frontend developer, using HTML, CSS, JavaScript, TypeScript, ReactJS, and NextJS. I have been involved with the Computer Science Student Association, in the field of information and technology, as an audio podcast editor and the Google Developer Student Clubs Community at Muhammadiyah University of Malang, which I join with the goal of expanding my knowledge and gaining experience.",
       },
     ],
   },
